ddadd3c8882249ad1c1f814bb07eff7e7e1222d1
abel533 <abel533@gmail.com>
2018-04-19 10:50:51 +0800

    增加无状态例子

---
+++ drools-simple/src/main/java/com/github/abel533/drools/stateless/Application.java
@@ -0,0 +1,24 @@
+package com.github.abel533.drools.stateless;
+
+import java.util.Date;
+
+public class Application {
+    private Date dateApplied;
+    private boolean valid = true;
+
+    public Date getDateApplied() {
+        return dateApplied;
+    }
+
+    public void setDateApplied(Date dateApplied) {
+        this.dateApplied = dateApplied;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+}
---
+++ drools-simple/src/main/java/com/github/abel533/drools/stateless/StatelessExample.java
@@ -0,0 +1,32 @@
+package com.github.abel533.drools.stateless;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.StatelessKieSession;
+
+import java.util.Arrays;
+
+public class StatelessExample {
+    public static void main(String[] args) {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kContainer = kieServices.getKieClasspathContainer();
+        StatelessKieSession kSession = kContainer.newStatelessKieSession();
+        Applicant applicant = new Applicant( "Mr John Smith", 16 );
+        Application application = new Application();
+        assertTrue( application.isValid() );
+        kSession.execute( Arrays.asList( new Object[] { application, applicant } ) );
+        assertFalse( application.isValid() );
+    }
+
+    private static void assertFalse(boolean valid) {
+        if(valid){
+            throw new RuntimeException("预期为 false, 但值为 true");
+        }
+    }
+
+    private static void assertTrue(boolean valid) {
+        if(!valid){
+            throw new RuntimeException("预期为 true, 但值为 false");
+        }
+    }
+}
